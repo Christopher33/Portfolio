3c4b0e1b7da7bc7faa11694c4ab751735d1026f4
Christopher33 <christopher.couty@lapiscine.pro>
2020-11-20 13:12:53 +0100

    10th commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const animations = document.querySelectorAll('.parallax')
+const box_doc = document.querySelector('.box_doc')
+const big_lamp = document.querySelector('.big_lamp')
+const shadow = document.querySelector('.doc2')
 
 let tot = 1;
 
@@ -33,18 +36,51 @@ window.addEventListener('mousewheel', (e) => {
 
 function animate(t) {
     if(t == 1) {
+
         console.log("page 1")
         animations.forEach( (animation) => {
             animation.classList.remove("up")
             animation.classList.add("down")
         })
+
+        big_lamp.classList.remove("left")
+        box_doc.classList.remove("right")
+        shadow.classList.remove("doc")
+
+        shadow.classList.add("doc_back")
+        big_lamp.classList.add("left_back")
+        box_doc.classList.add("right_back")
+
+
     } else if(t == 2) {
+
         console.log("page 2")        
         animations.forEach( (animation) => {
             animation.classList.remove("down")
             animation.classList.add("up")
         })
+
+        big_lamp.classList.remove("left_back")
+        box_doc.classList.remove("right_back")
+        shadow.classList.remove("doc_back")
+
+        big_lamp.classList.add("left")
+        box_doc.classList.add("right")
+        shadow.classList.add("doc")
+
     } else if(t ==3) {
-        console.log("page 3")        
+
+        console.log("page 3") 
+
+        big_lamp.classList.remove("left")
+        box_doc.classList.remove("right")
+        shadow.classList.remove("doc")
+
+        shadow.classList.add("doc_back")
+        big_lamp.classList.add("left_back")
+        box_doc.classList.add("right_back")
+
+
+
     }
 }
