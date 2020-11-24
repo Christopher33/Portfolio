d89aaccdbc7be9777d55d061cd4c75a6477a2823
Christopher33 <christopher.couty@lapiscine.pro>
2020-11-24 14:04:57 +0100

    12th commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const portfolio = document.querySelector('.portfolio')
 const animations = document.querySelectorAll('.parallax')
 const box_doc = document.querySelector('.box_doc')
 const big_lamp = document.querySelector('.big_lamp')
@@ -6,6 +7,7 @@ const about = document.querySelector('.about')
 
 let tot = 1;
 let i = 0;
+
 
 document.addEventListener("mousemove", (e) => {
     document.querySelectorAll('.parallax').forEach(parallax => {
@@ -18,18 +20,17 @@ document.addEventListener("mousemove", (e) => {
         const v = y.toFixed(1)
 
         parallax.style.transform = 'translate('+ h +'px, ' + v +'px)'
-
     })
 }, false);
 
 
 window.addEventListener('mousewheel', (e) => {
     if (tot <= 2 && e.deltaY > 0) {
-        tot++;
-        animate(tot);
+        tot++
+        animate(tot)
     } else if (tot >= 2 && e.deltaY < 0) {
-        tot--;
-        animate(tot);
+        tot--
+        animate(tot)
     } else {}
 })
 
@@ -42,54 +43,52 @@ function animate(t) {
             animation.classList.remove("up")
             animation.classList.add("down")
         })
-
+        
+        portfolio.classList.remove("P_folio_back")
         big_lamp.classList.remove("left")
         box_doc.classList.remove("right")
         shadow.classList.remove("doc")
-
+        
         shadow.classList.add("doc_back")
         big_lamp.classList.add("left_back")
-        box_doc.classList.add("right_back")
-
-
+        box_doc.classList.add("right_back")    
+        portfolio.classList.add("P_folio")
+        
     } else if(t == 2) {
-
+        
         console.log("page 2")        
         animations.forEach( (animation) => {
             animation.classList.remove("down")
             animation.classList.add("up")
         })
-
+        portfolio.classList.remove("P_folio")
         big_lamp.classList.remove("left_back")
         box_doc.classList.remove("right_back")
         shadow.classList.remove("doc_back")
         about.classList.remove("ani_about")
-
+        
+        portfolio.classList.add("P_folio_back")
         big_lamp.classList.add("left")
         box_doc.classList.add("right")
         shadow.classList.add("doc")
-
+        
         if( i >= 1 ) {
             about.classList.add("ani_about_back")
         }
         i++;
-        console.log(i)
-
+        
     } else if(t ==3) {
-
+        
         console.log("page 3") 
-
+        
         big_lamp.classList.remove("left")
         box_doc.classList.remove("right")
         shadow.classList.remove("doc")
-
         about.classList.remove("ani_about_back")
-
+        
         shadow.classList.add("doc_back")
         big_lamp.classList.add("left_back")
         box_doc.classList.add("right_back")
-
         about.classList.add("ani_about")
-
     }
 }
