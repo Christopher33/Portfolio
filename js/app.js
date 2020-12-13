155f5a31dc528ad4a8d4cc9a1a9df2c509291f7c
Christopher33 <christopher.couty@lapiscine.pro>
2020-12-13 10:00:22 +0100

    23th commit

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ let tot = 1
 let i = 0
 let width
 let lastY
+let y = 0
+
 
 // __________________________Mouving light first page______________________
 document.addEventListener("mousemove", (e) => {
@@ -60,103 +62,129 @@ function closePaper() {
 }
 
 // ___________________________________counting page___________________________
-window.addEventListener('mousewheel', (e) => {
+window.addEventListener('wheel' || 'touchmove', (e) => {
 
-    if(tot === 11) {
-        tot = 11
-    } 
-    else if (tot <= 2 && e.deltaY > 0) {
-        tot++
-        animate(tot)
-    } else if (tot >= 2 && e.deltaY < 0) {
-        tot--
-        animate(tot)
-    }
-    console.log(tot)
+    onScroll(e)
 })
+
+
+function onScroll(e) {
+
+    if( y === 0){
+
+        if(tot === 11) {
+            tot = 11
+        } 
+        else if (tot <= 2 && e.deltaY > 0) {
+            tot++
+            animate(tot)
+            y = 1
+        } else if (tot >= 2 && e.deltaY < 0) {
+            tot--
+            animate(tot)
+            y = 1
+        }        
+        
+    } else {
+
+    }
+}
 
 window.addEventListener('touchmove', (e) => {
 
     let i = e.touches[0].clientY;
     let currentY = Math.round(i / 100) * 100
 
-    if(tot === 11){
-        tot = 11
-    }
-    else if(tot <= 2 && currentY > lastY){
-        tot++
-        animate(tot)
-    }else if(tot >= 2 && currentY < lastY){
-        tot--
-        animate(tot)
-    }
-    lastY = currentY;
+    if( y === 0){
 
-    console.log(currentY)
+        if(tot === 11){
+            tot = 11
+        }
+        else if(tot <= 2 && currentY > lastY){
+            tot++
+            animate(tot)
+            y = 1
+        }else if(tot >= 2 && currentY < lastY){
+            tot--
+            animate(tot)
+            y = 1
+        }
+        lastY = currentY;
+
+    } else {
+
+    }
+
 })
 
 
 // _____________________________function animation pages________________________
-function animate(t) {
-    if(t == 1) {
 
-        animations.forEach( (animation) => {
-            animation.classList.remove("up")
-            animation.classList.add("down")
-        })
-        pageTwo.classList.remove("openCross")
-        portfolio.classList.remove("P_folio_back")
-        big_lamp.classList.remove("left")
-        box_doc.classList.remove("right")
-        shadow.classList.remove("doc")
-        
-        pageOne.classList.add("openCross")
-        shadow.classList.add("doc_back")
-        big_lamp.classList.add("left_back")
-        box_doc.classList.add("right_back")    
-        portfolio.classList.add("P_folio")
-        
-    } else if(t == 2) {
-
-        animations.forEach( (animation) => {
-            animation.classList.remove("down")
-            animation.classList.add("up")
-        })
-        pageOne.classList.remove("openCross")
-        pageThree.classList.remove("openCross")
-        portfolio.classList.remove("P_folio")
-        big_lamp.classList.remove("left_back")
-        box_doc.classList.remove("right_back")
-        shadow.classList.remove("doc_back")
-        about.classList.remove("ani_about")
-        about_me.classList.remove("look")
-        
-        pageTwo.classList.add("openCross")
-        portfolio.classList.add("P_folio_back")
-        big_lamp.classList.add("left")
-        box_doc.classList.add("right")
-        shadow.classList.add("doc")
-        
-        if( i >= 1 ) {
-            about.classList.add("ani_about_back")
+// setTimeout(
+    function animate(t) {
+        if(t == 1) {
+    
+            animations.forEach( (animation) => {
+                animation.classList.remove("up")
+                animation.classList.add("down")
+            })
+            pageTwo.classList.remove("openCross")
+            portfolio.classList.remove("P_folio_back")
+            big_lamp.classList.remove("left")
+            box_doc.classList.remove("right")
+            shadow.classList.remove("doc")
+            
+            pageOne.classList.add("openCross")
+            shadow.classList.add("doc_back")
+            big_lamp.classList.add("left_back")
+            box_doc.classList.add("right_back")    
+            portfolio.classList.add("P_folio")
+            
+        } else if(t == 2) {
+    
+            animations.forEach( (animation) => {
+                animation.classList.remove("down")
+                animation.classList.add("up")
+            })
+            pageOne.classList.remove("openCross")
+            pageThree.classList.remove("openCross")
+            portfolio.classList.remove("P_folio")
+            big_lamp.classList.remove("left_back")
+            box_doc.classList.remove("right_back")
+            shadow.classList.remove("doc_back")
+            about.classList.remove("ani_about")
+            about_me.classList.remove("look")
+            
+            pageTwo.classList.add("openCross")
+            portfolio.classList.add("P_folio_back")
+            big_lamp.classList.add("left")
+            box_doc.classList.add("right")
+            shadow.classList.add("doc")
+            
+            if( i >= 1 ) {
+                about.classList.add("ani_about_back")
+            }
+            i++;
+            
+        } else if(t ==3) {
+            
+            pageTwo.classList.remove("openCross")
+            big_lamp.classList.remove("left")
+            box_doc.classList.remove("right")
+            shadow.classList.remove("doc")
+            about.classList.remove("ani_about_back")
+            
+            pageThree.classList.add("openCross")
+            shadow.classList.add("doc_back")
+            big_lamp.classList.add("left_back")
+            box_doc.classList.add("right_back")
+            about.classList.add("ani_about")
+            about_me.classList.add("look")
         }
-        i++;
-        
-    } else if(t ==3) {
-        
-        pageTwo.classList.remove("openCross")
-        big_lamp.classList.remove("left")
-        box_doc.classList.remove("right")
-        shadow.classList.remove("doc")
-        about.classList.remove("ani_about_back")
-        
-        pageThree.classList.add("openCross")
-        shadow.classList.add("doc_back")
-        big_lamp.classList.add("left_back")
-        box_doc.classList.add("right_back")
-        about.classList.add("ani_about")
-        about_me.classList.add("look")
+
+        setTimeout( () => {
+            y = 0
+        }, 1000)
     }
-}
 
 
